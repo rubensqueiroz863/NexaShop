506c687b78ddc5ee9a01c73b916e007ee2f454dc
rubensqueiroz863 <rubensqueiroz863@exemplo.com>
2026-01-30 19:57:21 -0300

    fix(footer): Pequenos ajustes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Footer() {
 
   return (
-    <div className="flex items-center gap-2 flex-col text-[16px] md:text-[18px] md:px-0 px-16 justify-center pt-16 pb-12">
-      <p className="flex gap-2 items-center text-(--text-main)/80 dark:text-(--text-main)/80">
-        Built with:
+    <div className="flex items-center w-full gap-2 flex-col text-[12px] md:text-[18px] md:px-0 px-16 justify-center pt-16 pb-12">
+      <div className="flex gap-1.5 items-center w-full text-(--text-main)/80 dark:text-(--text-main)/80">
+        <p className="w-20">Built with:</p>
         <Image
           src={"https://i.postimg.cc/Zqc6YScN/nextjs-original.png"}
           width={128}
@@ -13,7 +13,7 @@ export default function Footer() {
           alt="Nextjs logo"
           className="w-5 h-5"
         />
-        Next.js
+        <p>Next.js</p>
         <Image
           src={"https://i.postimg.cc/Yqd9SSKg/favicon-4.png"}
           width={128}
@@ -21,16 +21,16 @@ export default function Footer() {
           alt="Nextjs logo"
           className="w-5 h-5"
         />
-        Vercel
+        <p>Vercel</p>
         <Image
           src={"https://i.postimg.cc/bNpFY43B/spring-boot-icon.webp"}
           width={128}
           height={128}
-          alt="Nextjs logo"
-          className="w-4.5 h-4.5"
+          alt="Spirng boot logo"
+          className="w-4 h-4"
         />
-        Spring Boot
-      </p>
+        <p className="w-20">SpringBoot</p>
+      </div>
       <p className="text-sm text-center text-(--text-secondary)">
         Direitos autorais © Rubens Q. ALves 2026. Todos os direitos reservados.
       </p>
