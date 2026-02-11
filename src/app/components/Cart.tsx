ce708c4c4edc5ccc4f6616dc2073dd50e4ac01c4
rubensqueiroz863 <rubensqueiroz863@gmail.com>
2026-02-10 22:43:41 -0300

    fix(cart): Ajuste do theme no cart

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Cart() {
-  const { theme } = useTheme();
+  const { resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -21,14 +21,13 @@ export default function Cart() {
     >
       {mounted && (
         <Image
-          src={theme === "dark" ? "https://i.postimg.cc/VN22vfk0/imag32es-removebg-preview.png" : "https://i.postimg.cc/zfbSJCPG/25619.png"}
+          src={resolvedTheme === "dark" ? "https://i.postimg.cc/VN22vfk0/imag32es-removebg-preview.png" : "https://i.postimg.cc/zfbSJCPG/25619.png"}
           width={128}
           height={128}
           alt="Cart button"
           className="w-6 h-6"
         />
       )}
-      
     </button>
   );
 }
