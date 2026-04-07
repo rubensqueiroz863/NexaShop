908432052dd23731ca62214983b2ebeb520db6db
rubensqueiroz863 <rubensqueiroz863@exemplo.com>
2026-04-07 19:44:42 -0300

    fix(logo): Sistema de logo ajustado e refatoração de design

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,13 +1,15 @@
-import { ArchivoBlack } from "@/lib/fonts";
+import { montserrat, Inter } from "@/lib/fonts";
 import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Link 
-      href="/"
-      className={`${ArchivoBlack.className} font-bold hidden md:flex text-(--text-light) text-[12px] xl:text-[18px] md:text-[16px]`}
-    >
-      NexaShop
+    <Link href={"/"} className="flex xl:text-xl items-center">
+      <span className={`${montserrat.className} font-bold text-[#00ffe1] neon`}>
+        Nexora
+      </span>
+      <span className={`${Inter.className} text-white`}>
+        ShopX
+      </span>
     </Link>
   );
 }
